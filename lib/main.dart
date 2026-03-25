a5d6880fcb95cb5b0dbd8ca9438e8a7e5076b00a
Sohag <ahadulsohag@gmail.com>
2026-03-26 01:10:17 +0600

    handling user input

--- lib/main.dart
+++ lib/main.dart
@@ -35,8 +35,8 @@ class GamePage extends StatelessWidget {
           for (var guess in _game.guesses)
             Row(
               spacing: 5.0,
-              children: [for (var letter in guess)
-                Tile(letter.char, letter.type)
+              children: [
+                for (var letter in guess) Tile(letter.char, letter.type),
               ],
             ),
         ],
@@ -52,17 +52,53 @@ class Tile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 60,
+      width: 60.0,
       height: 60,
       decoration: BoxDecoration(
         border: Border.all(color: Colors.grey.shade300),
-        color: switch (hitType){
+        color: switch (hitType) {
           HitType.hit => Colors.green,
           HitType.partial => Colors.yellow,
           HitType.miss => Colors.grey,
           _ => Colors.white,
         },
       ),
+    );
+  }
+}
+
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+
+  final void Function(String) onSubmitGuess;
+  final TextEditingController _textEditingController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 5,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(35)),
+                ),
+              ),
+              controller: _textEditingController,
+              autofocus: true,
+              focusNode: _focusNode,
+              onSubmitted: (String input) {
+                print(_textEditingController.text);
+                _textEditingController.clear();
+                _focusNode.requestFocus();
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
