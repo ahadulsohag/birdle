d47b054261c649ad7dc2e01a9a2c65ee51b435a5
Sohag <ahadulsohag@gmail.com>
2026-03-25 22:56:51 +0600

    tile

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,39 @@ class GamePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Column(spacing: 5.0, children: [
-          
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (var guess in _game.guesses)
+            Row(
+              spacing: 5.0,
+              children: [for (var letter in guess)
+                Tile(letter.char, letter.type)
+              ],
+            ),
         ],
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+  final String letter;
+  final HitType hitType;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType){
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
       ),
     );
   }
